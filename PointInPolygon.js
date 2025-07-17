6e9f43042158aa351189475b8a156fcb624f010f
Numuruzero <35202735+Numuruzero@users.noreply.github.com>
2025-07-17 13:14:14 -0500

    Create PointInPolygon.js
    Archived from missing GH repo under MIT license for personal use

---
+++ PointInPolygon.js
@@ -0,0 +1,41 @@
+/*Stolen mostly whole-cloth from the point-in-polygon implementation published by Substack: https://github.com/substack/point-in-polygon or https://www.npmjs.com/package/point-in-polygon. Github repo returns 404 so archiving for personal use just in case.*/
+
+function pointInPolygonNested (point, vs, start, end) {
+    var x = point[0], y = point[1];
+    var inside = false;
+    if (start === undefined) start = 0;
+    if (end === undefined) end = vs.length;
+    var len = end - start;
+    for (var i = 0, j = len - 1; i < len; j = i++) {
+        var xi = vs[i+start][0], yi = vs[i+start][1];
+        var xj = vs[j+start][0], yj = vs[j+start][1];
+        var intersect = ((yi > y) !== (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    return inside;
+};
+
+function pointInPolygonFlat (point, vs, start, end) {
+    var x = point[0], y = point[1];
+    var inside = false;
+    if (start === undefined) start = 0;
+    if (end === undefined) end = vs.length;
+    var len = (end-start)/2;
+    for (var i = 0, j = len - 1; i < len; j = i++) {
+        var xi = vs[start+i*2+0], yi = vs[start+i*2+1];
+        var xj = vs[start+j*2+0], yj = vs[start+j*2+1];
+        var intersect = ((yi > y) !== (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    return inside;
+};
+
+function pointInPolygon (point, vs, start, end) {
+    if (vs.length > 0 && Array.isArray(vs[0])) {
+        return pointInPolygonNested(point, vs, start, end);
+    } else {
+        return pointInPolygonFlat(point, vs, start, end);
+    }
+}
